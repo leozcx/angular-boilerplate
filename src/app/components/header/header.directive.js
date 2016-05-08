b0e1e10a1bff83c7c3eba848fafa9d5fbbb816bf
zcx8532 <zcx8532@gmail.com>
2016-05-08 21:47:27 +0800

    add login dropdown menu

--- src/app/components/header/header.directive.js
+++ src/app/components/header/header.directive.js
@@ -1,29 +1,50 @@
 (function() {
-	'use strict'
-	
-	var module = angular.module('header', ['pascalprecht.translate']);
-	
-	module.directive('didiHeader', [function() {
-		var directive = {
-			restrict: 'E',
-			templateUrl: 'app/components/header/header.html',
-			controllerAs : 'vm',
-			bindToController : true,
-			controller: function() {
-				var vm = this;
-				vm.username = "Test";
-			}
-		};
-		
-		return directive;
-	}]);
-	
-	module.config(['$translateProvider', function($translateProvider) {
-		$translateProvider.translations('zh', {
-	        'TITLE': '客服&体验数据平台',
-	        'SIGNOUT': '退出'
-	      });
-	     
-	      $translateProvider.preferredLanguage('zh');
-	}]);
+    'use strict'
+
+    var module = angular.module('header', ['pascalprecht.translate']);
+
+    module.directive('didiHeader', [function() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/components/header/header.html',
+            controllerAs : 'vm',
+            bindToController : true,
+            controller: ['$scope', '$log', function($scope, $log) {
+                var vm = this;
+                vm.username = "Test";
+                $scope.items = [
+                    'The first choice!',
+                    'And another choice for you.',
+                    'but wait! A third!'
+                ];
+
+                $scope.status = {
+                    isopen: false
+                };
+
+                $scope.toggled = function(open) {
+                    $log.log('Dropdown is now: ', open);
+                };
+
+                $scope.toggleDropdown = function($event) {
+                    $event.preventDefault();
+                    $event.stopPropagation();
+                    $scope.status.isopen = !$scope.status.isopen;
+                };
+
+                $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+            }]
+        };
+
+        return directive;
+    }]);
+
+    module.config(['$translateProvider', function($translateProvider) {
+        $translateProvider.translations('zh', {
+            'TITLE': '客服&体验数据平台',
+            'SIGNOUT': '退出'
+        });
+
+        $translateProvider.preferredLanguage('zh');
+    }]);
 })();
--- src/app/components/navbar/navbar.config.js
+++ src/app/components/navbar/navbar.config.js
@@ -1,33 +1,33 @@
 (function() {
-	'use strict';
-	angular.module('navBar').provider('navConfigProvider', navConfigProvider);
-	function navConfigProvider() {
-		this.$get = function() {
-			return this;
-		};
-		this.config = [
-						{
-							'pageId': 'report-management',
-							'pageDisplayName_key': 'Management',
-							'requiredPermits': [],
-							'hasChildren': false,
-							'hasIcon': true,
-							'href': "/#/manage",
-							'iconClass': 'new-icon new-icon-report',
-						},
-                                                {
-                                                    'pageId': 'l2',
-                                                    'pageDisplayName': 'Level 2 menu',
-                                                    'hasChildren': true,
-                                                    'iconClass': 'test',
-                                                    'children': [
-                                                    {
-                                                        'pageId': 'login',
-                                                        'pageDisplayName': 'Login',
-                                                        'href': '/#/login'
-                                                    }
-                                                    ]
-                                                }
-					];
-	}
+    'use strict';
+    angular.module('navBar').provider('navConfigProvider', navConfigProvider);
+    function navConfigProvider() {
+        this.$get = function() {
+            return this;
+        };
+        this.config = [
+        {
+            'pageId': 'report-management',
+            'pageDisplayName_key': 'Management',
+            'requiredPermits': [],
+            'hasChildren': false,
+            'hasIcon': true,
+            'href': "/#/manage",
+            'iconClass': 'glyphicon glyphicon-cog',
+        },
+        {
+            'pageId': 'l2',
+            'pageDisplayName': 'Level 2 menu',
+            'hasChildren': true,
+            'iconClass': 'test',
+            'children': [
+            {
+                'pageId': 'login',
+                'pageDisplayName': 'Login',
+                'href': '/#/signin'
+            }
+            ]
+        }
+        ];
+    }
 })();
